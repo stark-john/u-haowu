06d4305cd5cc70a694d13a549f007bbfda1f75a0
wangcy_oct <18895361867@163.com>
2019-12-31 14:41:33 +0800

    更改git名字

--- src/views/home/UI.js
+++ src/views/home/UI.js
@@ -4,9 +4,11 @@ class Com extends React.Component {
   render () {
     return (
       <div className = "box">
-        <header className = "header">Home头部</header>
-        <div className = "content">Home内容</div>
+        {/* <header className = "header">Home头部</header> */}
+        {/* <div className = "content">Home内容</div> */}
         <div>call me maybe</div>
+        <div>我现在想改掉我的git名字</div>
+        <h5>我现在的git名字叫wangcy_oct</h5>
       </div>
     )
   }
